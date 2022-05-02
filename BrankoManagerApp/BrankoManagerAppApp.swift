de0dba6363fa68a43b0cf2e9943dbb2e7e4d3286
micmacsolutionspvtltd <ramkumar@micandmac.net>
2022-05-02 14:10:42 +0530

    Initial Commit

---
+++ BrankoManagerApp/BrankoManagerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrankoManagerAppApp.swift
+//  BrankoManagerApp
+//
+//  Created by MAC on 02/05/22.
+//
+
+import SwiftUI
+
+@main
+struct BrankoManagerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
